76c9f9ad1ebe22a6615508e948ea25336519df0d
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2025-02-15 02:47:26 +0200

    init commit: - set up app - add FSD - add Shadcn + TailwindCSS + SCSS

---
+++ README.md
@@ -0,0 +1,3 @@
+# beats.pad.
+
+## Beats pad helps artists/songwriters write lyrics while syncing them to an instrumental
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shared/ui";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h2>beats.pad.</h2>
+      <Button variant="outline">Create new song</Button>
+    </div>
+  );
+}
